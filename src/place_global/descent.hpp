1deea6e164130c1ce7707f81a6f3e235bce42486
Gabriel Gouvine <gabriel.gouvine_GIT@m4x.org>
2022-03-08 19:47:48 +0100

    Line search in gradient descent

--- src/place_global/descent.hpp
+++ src/place_global/descent.hpp
@@ -4,4 +4,10 @@
 
 enum class DescentModel { HPWL, LSE, WA, Proximal };
 
-xt::xtensor<float, 1> gradientDescent(const NetTopology &topo, xt::xtensor<float, 1> initial, DescentModel model, int nbSteps, float stepSize, float momentum=0.0, float smoothing=1.0);
+xt::xtensor<float, 1> gradientDescentFixedStep(const NetTopology &topo, xt::xtensor<float, 1> initial, DescentModel model, int nbSteps, float stepSize, float momentum=0.0, float smoothing=1.0);
+
+xt::xtensor<float, 1> gradientDescent(const NetTopology &topo, xt::xtensor<float, 1> initial, DescentModel model, int nbSteps, float momentum=0.0, float smoothing=1.0, float initialStepSize=1.0, float stepVariation=0.9);
+
+xt::xtensor<float, 1> conjugateGradientDescent(const NetTopology &topo, xt::xtensor<float, 1> initial, DescentModel model, int nbSteps, float smoothing=1.0, float initialStepSize=1.0, float stepVariation=0.9);
+
+xt::xtensor<float, 1> nesterovGradientDescent(const NetTopology &topo, xt::xtensor<float, 1> initial, DescentModel model, int nbSteps, float smoothing=1.0, float initialStepSize=1.0, float stepVariation=0.9);
