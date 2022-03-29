e99534517affb436f86b1b1c536e8f76b7688812
Gabriel Gouvine <gabriel.gouvine_GIT@m4x.org>
2022-03-29 11:00:13 +0200

    Draft of density legalizer

---
+++ src/place_global/density_legalizer.cpp
@@ -0,0 +1,263 @@
+
+#include "place_global/density_legalizer.hpp"
+
+#include <cassert>
+#include <cmath>
+#include <algorithm>
+
+namespace {
+float distance(float x1, float y1, float x2, float y2, LegalizationModel leg) {
+    float dx = x1 - x2;
+    float dy = y1 - y2;
+    switch (leg) {
+        case LegalizationModel::L1:
+        return std::abs(dx) + std::abs(dy);
+        case LegalizationModel::L2:
+        return std::sqrt(dx * dx + dy * dy);
+        case LegalizationModel::LInf:
+        return std::max(std::abs(dx), std::abs(dy));
+        default:
+        return dx * dx + dy * dy;
+    }
+}
+}
+
+DensityLegalizer::DensityLegalizer(Rectangle area, int binsX, int binsY, std::vector<int> cellDemand, std::vector<float> cellTargetX, std::vector<float> cellTargetY) {
+    nbBinsX_ = binsX;
+    nbBinsY_ = binsY;
+    for (int i = 0; i < binsX; ++i) {
+        float n = 2 * i + 1;
+        binX_.push_back((n * area.minX + (2 * binsX - n) * area.maxX) / (2 * binsX));
+    }
+    for (int i = 0; i < binsY; ++i) {
+        float n = 2 * i + 1;
+        binY_.push_back((n * area.minY + (2 * binsY - n) * area.maxY) / (2 * binsY));
+    }
+    // TODO: correctly subdivide the capacity
+    long long capacity = area.area() / (binsX * binsY);
+    binCapacity_.assign(binsX, std::vector<long long>(binsY, capacity));
+    cellDemand_ = cellDemand;
+    cellTargetX_ = cellTargetX;
+    cellTargetY_ = cellTargetY;
+}
+
+float DensityLegalizer::distL1() const {
+    float disp = 0.0f;
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            float x = binX_[i];
+            float y = binY_[i];
+            for (int c : binCells_[i][j]) {
+                float dx = cellTargetX_[c] - x;
+                float dy = cellTargetY_[c] - y;
+                disp += cellDemand_[c] * (std::abs(dx) + std::abs(dy));
+            }
+        }
+    }
+    return disp;
+}
+
+float DensityLegalizer::distLInf() const {
+    float disp = 0.0f;
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            float x = binX_[i];
+            float y = binY_[i];
+            for (int c : binCells_[i][j]) {
+                float dx = cellTargetX_[c] - x;
+                float dy = cellTargetY_[c] - y;
+                disp += cellDemand_[c] * std::max(std::abs(dx), std::abs(dy));
+            }
+        }
+    }
+    return disp;
+}
+
+float DensityLegalizer::distL2Squared() const {
+    float disp = 0.0f;
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            float x = binX_[i];
+            float y = binY_[i];
+            for (int c : binCells_[i][j]) {
+                float dx = cellTargetX_[c] - x;
+                float dy = cellTargetY_[c] - y;
+                disp += cellDemand_[c] * (dx * dx + dy * dy);
+            }
+        }
+    }
+    return disp;
+}
+
+float DensityLegalizer::distL2() const {
+    return std::sqrt(distL2Squared());
+}
+
+void DensityLegalizer::check() const {
+    assert (binCapacity_.size() == nbBinsX());
+    for (const auto &bc : binCapacity_) {
+        assert (bc.size() == nbBinsY());
+    }
+    assert (binX_.size() == nbBinsX());
+    assert (binY_.size() == nbBinsY());
+    assert (cellDemand_.size() == nbCells());
+    assert (cellTargetX_.size() == nbCells());
+    assert (cellTargetY_.size() == nbCells());
+    // Check that every cell is present exactly once
+    std::vector<char> placed(nbCells());
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            for (int c : binCells_[i][j]) {
+                assert (!placed[c]);
+                placed[c] = true;
+            }
+        }
+    }
+    for (int c = 0; c < nbCells(); ++c) {
+        assert (placed[c]);
+    }
+}
+
+std::vector<std::vector<long long> > DensityLegalizer::binUsage() const {
+    std::vector<std::vector<long long> > ret(nbBinsX(), std::vector<long long>(nbBinsY()));
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            for (int c : binCells_[i][j]) {
+                ret[i][j] += cellDemand_[c];
+            }
+        }
+    }
+    return ret;
+}
+
+long long DensityLegalizer::totalCapacity() const {
+    long long ret = 0;
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            ret += binCapacity_[i][j];
+        }
+    }
+    return ret;
+}
+
+long long DensityLegalizer::totalDemand() const {
+    long long ret = 0;
+    for (int demand : cellDemand_) {
+        ret += demand;
+    }
+    return ret;
+}
+
+long long DensityLegalizer::totalOverflow() const {
+    long long ret = 0;
+    auto usage = binUsage();
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            ret += std::max(usage[i][j] - binCapacity_[i][j], 0LL);
+        }
+    }
+    return ret;
+}
+
+float DensityLegalizer::meanOverflow() const {
+    return totalOverflow() / (float) totalDemand();
+}
+
+float DensityLegalizer::rmsOverflow() const {
+    auto usage = binUsage();
+    float fact = 1.0f / totalDemand();
+    float ret = 0.0f;
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            float ovf = std::max(usage[i][j] - binCapacity_[i][j], 0LL) * fact;
+            ret += ovf * ovf;
+        }
+    }
+    return std::sqrt(ret);
+}
+
+std::vector<float> DensityLegalizer::simpleCoordX() const {
+    std::vector<float> ret(nbCells(), 0.0f);
+    for (int i = 0; i < nbBinsX_; ++i) {
+        for (int j = 0; j < nbBinsY_; ++j) {
+            for (int c : binCells_[i][j]) {
+                ret[c] = binX_[i];
+            }
+        }
+    }
+    return ret;
+}
+
+std::vector<float> DensityLegalizer::simpleCoordY() const {
+    std::vector<float> ret(nbCells(), 0.0f);
+    for (int i = 0; i < nbBinsY_; ++i) {
+        for (int j = 0; j < nbBinsX_; ++j) {
+            for (int c : binCells_[i][j]) {
+                ret[c] = binY_[i];
+            }
+        }
+    }
+    return ret;
+}
+
+std::pair<std::vector<int>, std::vector<int> > DensityLegalizer::bisect(float cx1, float cy1, float cx2, float cy2, long long capa1, long long capa2, std::vector<int> cells, LegalizationModel leg) {
+    // First sort the cells
+    // TODO: always add a secondary objective using squared distance
+    std::vector<std::pair<float, int> > cellCosts;
+    for (int c : cells) {
+        float x = cellTargetX_[c];
+        float y = cellTargetY_[c];
+        float cost = distance(x, y, cx1, cy1, leg) - distance(x, y, cx2, cy2, leg);
+        cellCosts.emplace_back(cost, c);
+    }
+    std::sort(cellCosts.begin(), cellCosts.end(), [](std::pair<float, int> a, std::pair<float, int> b) { return a.first < b.first; });
+    for (int i = 0; i < cells.size(); ++i) {
+        cells[i] = cellCosts[i].second;
+    }
+
+    // Ideal split: does it honor the capacity constraints?
+    // TODO
+
+    // Space-constrained split: one of the sides is full
+    int usageLeft = 0;
+    int capacityLeft = capa1;
+    int endSplit = 0;
+    while (endSplit < cells.size()) {
+        int demand = cellDemand_[cellCosts[endSplit].second];
+        usageLeft += demand;
+        if (usageLeft > capacityLeft) {
+            if (usageLeft - capacityLeft < demand / 2)
+                ++endSplit;
+            break;
+        }
+        ++endSplit;
+    }
+
+    // Implement the split
+    std::pair<std::vector<int>, std::vector<int> > ret;
+    for (int i = 0; i < endSplit; ++i) {
+        ret.first.push_back(cells[i]);
+    }
+    for (int i = endSplit; i < cells.size(); ++i) {
+        ret.second.push_back(cells[i]);
+    }
+    return ret;
+}
+
+void DensityLegalizer::rebisect(int x1, int y1, int x2, int y2, LegalizationModel leg) {
+    if (x1 == x2 && y1 == y2)
+        return;
+
+    // Get all cells
+    std::vector<int> cells;
+    cells.insert(cells.end(), binCells_[x1][y1].begin(), binCells_[x1][y1].end());
+    cells.insert(cells.end(), binCells_[x2][y2].begin(), binCells_[x2][y2].end());
+
+    auto b = bisect(
+        binX_[x1], binY_[y1], binX_[x2], binY_[y2],
+        binCapacity_[x1][y1], binCapacity_[x2][y2],
+        cells, leg
+    );
+    binCells_[x1][y1] = b.first;
+    binCells_[x2][y2] = b.second;
+}
---
+++ src/place_global/density_legalizer.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include "coloquinte.hpp"
+
+struct Rectangle {
+    Rectangle(int minX, int maxX, int minY, int maxY) : minX(minX), maxX(maxX), minY(minY), maxY(maxY) {}
+    int minX;
+    int maxX;
+    int minY;
+    int maxY;
+
+    int width() const { return maxX - minX; }
+    int height() const { return maxY - minY; }
+    long long area() const { return (long long) width() * (long long) height(); }
+};
+
+
+enum LegalizationModel { L1, L2, LInf, L2Squared };
+
+/**
+ * Representation of an almost legalized placement, where density constraints are met per bin
+ */
+class DensityLegalizer {
+  public:
+    DensityLegalizer(Rectangle area, int binsX, int binsY, std::vector<int> cellDemand, std::vector<float> cellTargetX, std::vector<float> cellTargetY);
+
+    int nbCells() const { return cellDemand_.size(); }
+    int nbBins() const { return nbBinsX_ * nbBinsY_; }
+    int nbBinsX() const { return nbBinsX_; }
+    int nbBinsY() const { return nbBinsY_; }
+    long long totalCapacity() const;
+    long long totalDemand() const;
+
+    void updateTargetX(std::vector<float> cellTargetX) { cellTargetX_ = cellTargetX; }
+    void updateTargetY(std::vector<float> cellTargetY) { cellTargetY_ = cellTargetY; }
+
+    // How much capacity is overflowing the bins
+    long long totalOverflow() const;
+    float meanOverflow() const;
+    float rmsOverflow() const;
+
+    // Quality metrics for the legalization
+    float distL1() const;
+    float distL2() const;
+    float distLInf() const;
+    float distL2Squared() const;
+
+    // Algorithms to create an initial legalization from scratch
+    void bisect(LegalizationModel model);
+
+    // Algorithms to improve the current legalization
+    void redistributeBisect(LegalizationModel model);
+    void redistributeX(LegalizationModel model);
+    void redistributeY(LegalizationModel model);
+
+    // Obtain coordinates for the cells from the legalization
+    std::vector<float> simpleCoordX() const;
+    std::vector<float> simpleCoordY() const;
+    // TODO: coordinates with spreading
+    std::vector<float> spreadCoordX() const;
+    std::vector<float> spreadCoordY() const;
+
+    void check() const;
+
+  private:
+    std::vector<std::vector<long long> > binUsage() const;
+    void rebisect(int x1, int y1, int x2, int y2, LegalizationModel leg);
+    std::pair<std::vector<int>, std::vector<int> > bisect(float cx1, float cy1, float cx2, float cy2, long long capa1, long long capa2, std::vector<int> cells, LegalizationModel leg);
+
+  private:
+    // Problem data
+    int nbBinsX_;
+    int nbBinsY_;
+    std::vector<std::vector<long long> > binCapacity_;
+    std::vector<float> binX_;
+    std::vector<float> binY_;
+    std::vector<int> cellDemand_;
+    std::vector<float> cellTargetX_;
+    std::vector<float> cellTargetY_;
+
+    // Problem status
+    std::vector<std::vector<std::vector<int> > > binCells_;
+};
+
+
+
