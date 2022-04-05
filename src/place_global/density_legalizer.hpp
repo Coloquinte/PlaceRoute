540a4996de7b7439c2a9a1cfd22ce5e6e09b23b3
Gabriel Gouvine <gabriel.gouvine_GIT@m4x.org>
2022-04-05 17:57:13 +0200

    Fixes

--- src/place_global/density_legalizer.hpp
+++ src/place_global/density_legalizer.hpp
@@ -22,17 +22,28 @@ enum LegalizationModel { L1, L2, LInf, L2Squared };
  */
 class DensityLegalizer {
   public:
-    DensityLegalizer(Rectangle area, int binsX, int binsY, std::vector<int> cellDemand, std::vector<float> cellTargetX, std::vector<float> cellTargetY);
+    DensityLegalizer(Rectangle area, int nbCells);
 
-    int nbCells() const { return cellDemand_.size(); }
+    // Accessors
+    int nbCells() const { return nbCells_; }
     int nbBins() const { return nbBinsX_ * nbBinsY_; }
     int nbBinsX() const { return nbBinsX_; }
     int nbBinsY() const { return nbBinsY_; }
+
     long long totalCapacity() const;
     long long totalDemand() const;
 
-    void updateTargetX(std::vector<float> cellTargetX) { cellTargetX_ = cellTargetX; }
-    void updateTargetY(std::vector<float> cellTargetY) { cellTargetY_ = cellTargetY; }
+    const std::vector<int> &cellDemand() const { return cellDemand_; }
+    const std::vector<float> &cellTargetX() const { return cellTargetX_; }
+    const std::vector<float> &cellTargetY() const { return cellTargetY_; }
+    const std::vector<std::vector<long long> > &binCapacity() const { return binCapacity_; }
+    std::vector<std::vector<long long> > binUsage() const;
+
+    // Problem updates
+    void updateBins(int binsX, int binsY);
+    void updateCellDemand(std::vector<int> cellDemand) { cellDemand_ = cellDemand; }
+    void updateCellTargetX(std::vector<float> cellTargetX) { cellTargetX_ = cellTargetX; }
+    void updateCellTargetY(std::vector<float> cellTargetY) { cellTargetY_ = cellTargetY; }
 
     // How much capacity is overflowing the bins
     long long totalOverflow() const;
@@ -62,16 +73,17 @@ class DensityLegalizer {
 
     void check() const;
 
-  private:
-    // Compute the current usage for each bin
-    std::vector<std::vector<long long> > binUsage() const;
+    // Utilities
+    static float distance(float x1, float y1, float x2, float y2, LegalizationModel leg);
 
+    // Bisection algorithm
+    std::pair<std::vector<int>, std::vector<int> > bisect(float cx1, float cy1, float cx2, float cy2, long long capa1, long long capa2, std::vector<int> cells, LegalizationModel leg);
+
+  private:
     // Redo the bisection for these bins
     void rebisect(int x1, int y1, int x2, int y2, LegalizationModel leg);
 
-    // Bisection algorithm and helpers
-    std::pair<std::vector<int>, std::vector<int> > bisect(float cx1, float cy1, float cx2, float cy2, long long capa1, long long capa2, std::vector<int> cells, LegalizationModel leg);
-
+    // Bisection algorithm helpers
     std::vector<std::pair<float, int> > computeCellCosts(float cx1, float cy1, float cx2, float cy2, std::vector<int> cells, LegalizationModel leg) const;
     int findIdealSplitPos(const std::vector<std::pair<float, int> > &cellCosts) const;
     int findConstrainedSplitPos(const std::vector<std::pair<float, int> > &cellCosts, int targetPos, long long capa1, long long capa2) const;
@@ -79,6 +91,8 @@ class DensityLegalizer {
 
   private:
     // Problem data
+    Rectangle area_;
+    int nbCells_;
     int nbBinsX_;
     int nbBinsY_;
     std::vector<std::vector<long long> > binCapacity_;
