8cce26032f485121cca9264338b88d0e3a965034
Gabriel Gouvine <gabriel.gouvine_GIT@m4x.org>
2022-04-07 14:23:58 +0200

    Clean overflow handling

--- test/test_density_legalizer.cpp
+++ test/test_density_legalizer.cpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <iostream>
 
+
 BOOST_AUTO_TEST_CASE(OneSide1) {
     Rectangle area(0, 1, 0, 1);
     int nbCells = 3;
@@ -154,3 +155,49 @@ BOOST_AUTO_TEST_CASE(Sorted2) {
         BOOST_CHECK(bisect.first[0] == 3 || bisect.first[1] == 3);
     }
 }
+
+BOOST_AUTO_TEST_CASE(LeastOverflow1) {
+    Rectangle area(0, 1, 0, 1);
+    int nbCells = 3;
+    std::vector<int> cellDemand = {4, 4, 4};
+    std::vector<float> cellTargetX = {0.0, 0.0, 0.0};
+    std::vector<float> cellTargetY = {0.0, 0.0, 0.0};
+    DensityLegalizer leg(area, nbCells);
+    leg.updateCellDemand(cellDemand);
+    leg.updateCellTargetX(cellTargetX);
+    leg.updateCellTargetY(cellTargetY);
+    auto bisect = leg.bisect(0.0, 0.0, 1.0, 0.0, 6LL, 7LL, {0, 1, 2}, LegalizationModel::L1);
+    BOOST_CHECK_EQUAL(bisect.first.size(), 1);
+    BOOST_CHECK_EQUAL(bisect.second.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(LeastOverflow2) {
+    Rectangle area(0, 1, 0, 1);
+    int nbCells = 4;
+    std::vector<int> cellDemand = {4, 4, 4, 4};
+    std::vector<float> cellTargetX = {0.0, 0.0, 0.0, 0.0};
+    std::vector<float> cellTargetY = {0.0, 0.0, 0.0, 0.0};
+    DensityLegalizer leg(area, nbCells);
+    leg.updateCellDemand(cellDemand);
+    leg.updateCellTargetX(cellTargetX);
+    leg.updateCellTargetY(cellTargetY);
+    auto bisect = leg.bisect(0.0, 0.0, 1.0, 0.0, 6LL, 7LL, {0, 1, 2, 3}, LegalizationModel::L1);
+    BOOST_CHECK_EQUAL(bisect.first.size(), 2);
+    BOOST_CHECK_EQUAL(bisect.second.size(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(LeastOverflow3) {
+    Rectangle area(0, 1, 0, 1);
+    int nbCells = 1;
+    std::vector<int> cellDemand = {4};
+    std::vector<float> cellTargetX = {0.0};
+    std::vector<float> cellTargetY = {0.0};
+    DensityLegalizer leg(area, nbCells);
+    leg.updateCellDemand(cellDemand);
+    leg.updateCellTargetX(cellTargetX);
+    leg.updateCellTargetY(cellTargetY);
+    auto bisect = leg.bisect(0.0, 0.0, 1.0, 0.0, 1LL, 2LL, {0}, LegalizationModel::L1);
+    BOOST_CHECK_EQUAL(bisect.first.size(), 0);
+    BOOST_CHECK_EQUAL(bisect.second.size(), 1);
+}
+
