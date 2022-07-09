e4d720023cc044b2316501496514d073ef28bedd
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2022-07-09 11:44:13 +0200

    Comments

--- src/coloquinte.hpp
+++ src/coloquinte.hpp
@@ -4,6 +4,19 @@
 #include <cmath>
 
 
+/**
+ * 2D point
+ */
+struct Point {
+    int x;
+    int y;
+
+    static int distance(Point a, Point b) { return std::abs(a.x - b.x) + std::abs(a.y - b.y); }
+};
+
+/**
+ * 2D rectangle
+ */
 struct Rectangle {
     Rectangle() : minX(0), maxX(0), minY(0), maxY(0) {}
     Rectangle(int minX, int maxX, int minY, int maxY) : minX(minX), maxX(maxX), minY(minY), maxY(maxY) {}
@@ -15,54 +28,24 @@ struct Rectangle {
     int width() const { return maxX - minX; }
     int height() const { return maxY - minY; }
     long long area() const { return (long long) width() * (long long) height(); }
+    bool intersects(Rectangle o) const {
+        return minX < o.maxX
+            && o.minX < maxX
+            && minY < o.maxY
+            && o.minY < maxY;
+    }
 };
 
+/**
+ * Cost model to use when doing legalization
+ */
 enum LegalizationModel { L1, L2, LInf, L2Squared };
 
-extern "C" {
-    void place_ispd(
-        int nb_cells,
-        int nb_nets,
-        int *cell_widths,
-        int *cell_heights,
-        char *cell_fixed,
-        int *net_limits,
-        int *pin_cells,
-        int *pin_x_offsets,
-        int *pin_y_offsets,
-        int *cell_x,
-        int *cell_y,
-        char *cell_flip_x,
-        char *cell_flip_y,
-        int min_x, int max_x, int min_y, int max_y
-    );
-    void benchmark_quadratic_models(
-        int nb_cells,
-        int nb_nets,
-        int *cell_widths,
-        int *cell_heights,
-        char *cell_fixed,
-        int *net_limits,
-        int *pin_cells,
-        int *pin_x_offsets,
-        int *pin_y_offsets,
-        int *cell_x,
-        int *cell_y,
-        char *cell_flip_x,
-        char *cell_flip_y,
-        int model_type,
-        int nb_steps,
-        float epsilon,
-        float relaxation
-    );
-}
-
+/**
+ * Representation of a flat circuit from an ISPD benchmark
+ */
 
 struct Circuit {
-    /**
-     * Representation of a circuit
-     */
-
     std::vector<int> cellWidths;
     std::vector<int> cellHeights;
     std::vector<char> cellFixed;
@@ -115,4 +98,43 @@ struct Circuit {
 
     void check() const;
 };
+
+extern "C" {
+    void place_ispd(
+        int nb_cells,
+        int nb_nets,
+        int *cell_widths,
+        int *cell_heights,
+        char *cell_fixed,
+        int *net_limits,
+        int *pin_cells,
+        int *pin_x_offsets,
+        int *pin_y_offsets,
+        int *cell_x,
+        int *cell_y,
+        char *cell_flip_x,
+        char *cell_flip_y,
+        int min_x, int max_x, int min_y, int max_y
+    );
+    void benchmark_quadratic_models(
+        int nb_cells,
+        int nb_nets,
+        int *cell_widths,
+        int *cell_heights,
+        char *cell_fixed,
+        int *net_limits,
+        int *pin_cells,
+        int *pin_x_offsets,
+        int *pin_y_offsets,
+        int *cell_x,
+        int *cell_y,
+        char *cell_flip_x,
+        char *cell_flip_y,
+        int model_type,
+        int nb_steps,
+        float epsilon,
+        float relaxation
+    );
+}
+
 
