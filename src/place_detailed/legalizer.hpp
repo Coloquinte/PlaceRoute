ebecf2becfd2cbea81dd06b781fc9fdf289947f3
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2022-07-19 14:28:23 +0200

    Doxygen-style comments

--- src/place_detailed/legalizer.hpp
+++ src/place_detailed/legalizer.hpp
@@ -3,23 +3,23 @@
 #include "coloquinte.hpp"
 
 /**
- * Algorithms to obtain a legal placement for standard cells
+ * @brief Algorithms to obtain a legal placement for standard cells
  *
  */
 class Legalizer {
  public:
   /**
-   * Initialize the datastructure from a circuit
+   * @brief Initialize the datastructure from a circuit
    */
   static Legalizer fromIspdCircuit(const Circuit &circuit);
 
   /**
-   * Export the placement obtained to the circuit datastructure
+   * @brief Export the placement obtained to the circuit datastructure
    */
   void exportPlacement(Circuit &circuit);
 
   /**
-   * Initialize the datastructure
+   * @brief Initialize the datastructure
    *      @param rows: Available rows for placement; must all be the right
    * height for the cells
    *      @param width: Width of the cells when placed in a row
@@ -30,91 +30,91 @@ class Legalizer {
             const std::vector<int> &targetX, const std::vector<int> &targetY);
 
   /**
-   * Return the number of rows
+   * @brief Return the number of rows
    */
   int nbRows() const { return rows_.size(); }
 
   /**
-   * Return the number of cells
+   * @brief Return the number of cells
    */
   int nbCells() const { return cellWidth_.size(); }
 
   /**
-   * Return the width of the cells
+   * @brief Return the width of the cells
    */
   const std::vector<int> &cellWidth() const { return cellWidth_; }
 
   /**
-   * Return the target x coordinates for legalization
+   * @brief Return the target x coordinates for legalization
    */
   const std::vector<int> &cellTargetX() const { return cellTargetX_; }
 
   /**
-   * Return the target y coordinates for legalization
+   * @brief Return the target y coordinates for legalization
    */
   const std::vector<int> &cellTargetY() const { return cellTargetY_; }
 
   /**
-   * Return the cost model used by the algorithm
+   * @brief Return the cost model used by the algorithm
    */
   LegalizationModel costModel() const { return costModel_; }
 
   /**
-   * Set the cost model used by the algorithm
+   * @brief Set the cost model used by the algorithm
    */
   void setCostModel(LegalizationModel m) { costModel_ = m; }
 
   /**
-   * Run the algorithm
+   * @brief Run the algorithm
    */
   void run();
 
   /**
-   * Compute the x coordinates after legalization
+   * @brief Compute the x coordinates after legalization
    */
   std::vector<int> cellLegalX() const;
 
   /**
-   * Compute the y coordinates after legalization
+   * @brief Compute the y coordinates after legalization
    */
   std::vector<int> cellLegalY() const;
 
   /**
-   * Check consistency of the datastructure
+   * @brief Check consistency of the datastructure
    */
   void check() const;
 
  private:
   /**
-   * Place a single cell optimally
+   * @brief Place a single cell optimally
    * Return true if successful
    */
   bool placeCellOptimally(int cell);
 
   /**
-   * Simulate placing a single cell in a given row
+   * @brief Simulate placing a single cell in a given row
    * Return a pair: true if successful and the X coordinate
    */
   std::pair<bool, int> placeCellOptimally(int cell, int row) const;
 
   /**
-   * Compute the ordering of the cells
+   * @brief Compute the ordering of the cells
    */
   std::vector<int> computeCellOrder(float weightX, float weightWidth,
                                     float weightY) const;
 
   /**
-   * Returns true if the cell is to be ignored by legalization
+   * @brief Returns true if the cell is to be ignored by legalization
    */
   bool isIgnored(int cell) const { return cellWidth_[cell] == -1; }
 
   /**
-   * Materialize the placement of a cell
+   * @brief Materialize the placement of a cell
    */
   void doPlacement(int cell, int row, int x);
 
   /**
-   * Undo the placement of a cell
+   * @brief Undo the placement of a cell
    */
   void undoPlacement(int cell);
 
