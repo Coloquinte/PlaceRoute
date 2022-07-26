b57389902442de4d1d79e6139085d9c9dc94e42f
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2022-07-26 16:52:01 +0200

    Draft for the newer net model

--- src/place_global/net_model.cpp
+++ src/place_global/net_model.cpp
@@ -56,42 +56,53 @@ void NetModel::addNet(const std::vector<int> &cells) {
 }
 
 void NetModel::addNet(const std::vector<int> &cells,
-                      const std::vector<float> &pinOffsets) {
-  addNet(cells, pinOffsets, std::numeric_limits<float>::infinity(),
-         -std::numeric_limits<float>::infinity());
+                      const std::vector<float> &pinOffsets, float weight) {
+  assert(cells.size() == pinOffsets.size());
+  if (cells.size() <= 1) {
+    return;
+  }
+  netLimits_.push_back(netLimits_.back() + cells.size());
+  netWeight_.push_back(weight);
+  netCells_.insert(netCells_.end(), cells.begin(), cells.end());
+  netPinOffsets_.insert(netPinOffsets_.end(), pinOffsets.begin(),
+                        pinOffsets.end());
 }
 
 void NetModel::addNet(const std::vector<int> &cells,
                       const std::vector<float> &pinOffsets, float minPin,
                       float maxPin, float weight) {
-  assert(cells.size() == pinOffsets.size());
-  if (cells.size() <= 1) {
+  if (cells.empty()) {
     return;
   }
-  netLimits_.push_back(cells.size());
-  netWeight_.push_back(weight);
-  netCells_.insert(netCells_.end(), cells.begin(), cells.end());
-  netPinOffsets_.insert(netPinOffsets_.end(), pinOffsets.begin(),
-                        pinOffsets.end());
-  netMinPin_.push_back(minPin);
-  netMaxPin_.push_back(maxPin);
+  if (std::isfinite(minPin)) {
+    std::vector<int> fCells = cells;
+    std::vector<float> fPinOffsets = pinOffsets;
+    fCells.push_back(-1);
+    fPinOffsets.push_back(minPin);
+    if (maxPin != minPin) {
+      fCells.push_back(-1);
+      fPinOffsets.push_back(maxPin);
+    }
+    addNet(fCells, fPinOffsets, weight);
+  } else {
+    addNet(cells, pinOffsets, weight);
+  }
 }
 
 void NetModel::check() const {
-  assert(netCells_.size() == pinOffsets_.size());
+  assert(netCells_.size() == netPinOffsets_.size());
   assert(netLimits_.size() == nbNets() + 1);
   assert(netWeight_.size() == nbNets());
-  assert(netMinPin_.size() == nbNets());
-  assert(netMaxPin_.size() == nbNets());
   for (int c : netCells_) {
-    assert(c >= 0);
+    assert(c >= -1);
     assert(c < nbCells());
   }
   for (int i = 0; i < nbNets(); ++i) {
-    assert(netLimits_[i] < netLimits_[i + 1]);
+    // At least two cells per net
+    assert(netLimits_[i] + 2 <= netLimits_[i + 1]);
   }
   assert(netLimits_.front() == 0);
-  assert(netLimits.back() == netCells_.size());
+  assert(netLimits_.back() == netCells_.size());
 }
 
 std::vector<float> NetModel::pinPositions(const std::vector<float> &pl) const {
@@ -100,4 +111,361 @@ std::vector<float> NetModel::pinPositions(const std::vector<float> &pl) const {
     ret[i] = pl[netCells_[i]] + netPinOffsets_[i];
   }
   return ret;
+}
+
+float NetModel::value(const std::vector<float> &pl) const {
+  std::vector<float> pos = pinPositions(pl);
+  float ret = 0.0f;
+  for (int i = 0; i < nbNets(); ++i) {
+    int b = netLimits_[i];
+    int e = netLimits_[i + 1];
+    float mn = std::numeric_limits<float>::infinity();
+    float mx = -std::numeric_limits<float>::infinity();
+    for (int j = b; j < e; ++j) {
+      mn = std::min(mn, pos[j]);
+      mx = std::min(mx, pos[j]);
+    }
+    ret += (mx - mn);
+  }
+  return ret;
+}
+
+std::tuple<int, int, float, float> NetModel::minPin(
+    int net, const std::vector<float> &pl) const {
+  int bestI = 0;
+  int bestC = -1;
+  float bestO = std::numeric_limits<float>::infinity();
+  float bestPos = bestO;
+  for (int i = 0; i < nbPins(net); ++i) {
+    float pos = pinPosition(net, i, pl);
+    if (pos < bestPos) {
+      bestI = i;
+      bestC = pinCell(net, i);
+      bestO = pinOffset(net, i);
+      bestPos = pos;
+    }
+  }
+  return std::make_tuple(bestI, bestC, bestO, bestPos);
+}
+
+std::tuple<int, int, float, float> NetModel::maxPin(
+    int net, const std::vector<float> &pl) const {
+  int bestI = 0;
+  int bestC = -1;
+  float bestO = -std::numeric_limits<float>::infinity();
+  float bestPos = bestO;
+  for (int i = 0; i < nbPins(net); ++i) {
+    float pos = pinPosition(net, i, pl);
+    if (pos > bestPos) {
+      bestI = i;
+      bestC = pinCell(net, i);
+      bestO = pinOffset(net, i);
+      bestPos = pos;
+    }
+  }
+  return std::make_tuple(bestI, bestC, bestO, bestPos);
+}
+
+/**
+ * Matrix creation for quadratic approximations
+ */
+class MatrixCreator {
+ public:
+  explicit MatrixCreator(const NetModel &topo)
+      : topo_(topo),
+        nbCells_(topo.nbCells()),
+        nbSupps_(0),
+        rhs_(topo.nbCells()),
+        initial_(topo.nbCells()) {}
+
+  int nbCells() const { return nbCells_; }
+  int matSize() const { return nbCells_ + nbSupps_; }
+
+  const std::vector<Eigen::Triplet<float> > &mat() const { return mat_; }
+  const std::vector<float> &rhs() const { return rhs_; }
+  const std::vector<float> &initial() const { return initial_; }
+
+  int addCell(float initialPos);
+
+  std::vector<float> solve();
+
+  static MatrixCreator createStar(const NetModel &topo);
+  static MatrixCreator createStar(const NetModel &topo,
+                                  const std::vector<float> &pl, float epsilon);
+  static MatrixCreator createB2B(const NetModel &topo,
+                                 const std::vector<float> &pl, float epsilon);
+
+  void addPenalty(const std::vector<float> &netPlacement,
+                  const std::vector<float> &placementTarget,
+                  const std::vector<float> &penaltyStrength,
+                  float cutoffDistance);
+
+  void addBipoint(int net);
+  void addClique(int net);
+  void addStar(int net);
+
+  void addBipoint(int net, const std::vector<float> &pl, float epsilon);
+  void addClique(int net, const std::vector<float> &pl, float epsilon);
+  void addStar(int net, const std::vector<float> &pl, float epsilon);
+  void addB2B(int net, const std::vector<float> &pl, float epsilon);
+
+  void check() const;
+
+ private:
+  void addPin(int c1, int c2, float offs1, float offs2, float weight);
+  void addMovingPin(int c1, int c2, float offs1, float offs2, float weight);
+  void addFixedPin(int c1, float offs1, float pos, float weight);
+
+ private:
+  const NetModel &topo_;
+  int nbCells_;
+  int nbSupps_;
+  std::vector<Eigen::Triplet<float> > mat_;
+  std::vector<float> rhs_;
+  std::vector<float> initial_;
+};
+
+void MatrixCreator::addMovingPin(int c1, int c2, float offs1, float offs2,
+                                 float weight) {
+  if (c1 == c2) return;
+  assert(c1 >= 0);
+  assert(c2 >= 0);
+  mat_.emplace_back(c1, c2, -weight);
+  mat_.emplace_back(c2, c1, -weight);
+  mat_.emplace_back(c1, c1, weight);
+  mat_.emplace_back(c2, c2, weight);
+  rhs_[c1] += weight * (offs2 - offs1);
+  rhs_[c2] += weight * (offs1 - offs2);
+}
+
+void MatrixCreator::addFixedPin(int c1, float offs1, float pos, float weight) {
+  assert(c1 >= 0);
+  mat_.emplace_back(c1, c1, weight);
+  rhs_[c1] += weight * (pos - offs1);
+}
+
+void MatrixCreator::addPin(int c1, int c2, float offs1, float offs2,
+                           float weight) {
+  if (c1 == c2) return;
+  if (c1 == -1) {
+    addFixedPin(c2, offs2, offs1, weight);
+    return;
+  }
+  if (c2 == -1) {
+    addFixedPin(c1, offs1, offs2, weight);
+    return;
+  }
+  addMovingPin(c1, c2, offs1, offs2, weight);
+}
+
+void MatrixCreator::addPenalty(const std::vector<float> &netPlacement,
+                               const std::vector<float> &placementTarget,
+                               const std::vector<float> &penaltyStrength,
+                               float cutoffDistance) {
+  assert(netPlacement.size() == nbCells());
+  assert(placementTarget.size() == nbCells());
+  assert(penaltyStrength.size() == nbCells());
+  for (int i = 0; i < nbCells_; ++i) {
+    float dist = std::abs(netPlacement[i] - placementTarget[i]);
+    float strength = penaltyStrength[i] / std::max(dist, cutoffDistance);
+    addFixedPin(i, 0.0f, 0.0f, strength);
+  }
+}
+int MatrixCreator::addCell(float initialPos) {
+  initial_.push_back(initialPos);
+  rhs_.push_back(0.0f);
+  int ret = nbCells_ + nbSupps_;
+  nbSupps_++;
+  return ret;
+}
+
+MatrixCreator MatrixCreator::createStar(const NetModel &topo) {
+  MatrixCreator ret(topo);
+  for (int i = 0; i < topo.nbNets(); ++i) {
+    ret.addStar(i);
+  }
+  return ret;
+}
+
+MatrixCreator MatrixCreator::createStar(const NetModel &topo,
+                                        const std::vector<float> &pl,
+                                        float epsilon) {
+  MatrixCreator ret(topo);
+  for (int i = 0; i < topo.nbNets(); ++i) {
+    ret.addStar(i, pl, epsilon);
+  }
+  return ret;
+}
+
+MatrixCreator MatrixCreator::createB2B(const NetModel &topo,
+                                       const std::vector<float> &pl,
+                                       float epsilon) {
+  MatrixCreator ret(topo);
+  for (int i = 0; i < topo.nbNets(); ++i) {
+    ret.addB2B(i, pl, epsilon);
+  }
+  return ret;
+}
+
+void MatrixCreator::addBipoint(int net) {
+  addPin(topo_.pinCell(net, 0), topo_.pinCell(net, 1), topo_.pinOffset(net, 0),
+         topo_.pinOffset(net, 1), topo_.netWeight(net));
+}
+
+void MatrixCreator::addClique(int net) {
+  int nb = topo_.nbPins(net);
+  float w = 2.0f * topo_.netWeight(net) / (nb * (nb - 1));
+  for (int i = 0; i + 1 < nb; ++i) {
+    for (int j = i + 1; j < nb; ++j) {
+      addPin(topo_.pinCell(net, i), topo_.pinCell(net, j),
+             topo_.pinOffset(net, i), topo_.pinOffset(net, j), w);
+    }
+  }
+}
+
+void MatrixCreator::addStar(int net) {
+  int nb = topo_.nbPins(net);
+  if (nb <= 2) {
+    addBipoint(net);
+  } else {
+    float w = topo_.netWeight(net) / nb;
+    int c = addCell(0.0f);
+    for (int i = 0; i < nb; ++i) {
+      addPin(topo_.pinCell(net, i), c, topo_.pinOffset(net, i), 0.0f, w);
+    }
+  }
+}
+
+void MatrixCreator::addBipoint(int net, const std::vector<float> &pl,
+                               float epsilon) {
+  float w = topo_.netWeight(net) /
+            std::max(epsilon, std::abs(topo_.pinPosition(net, 0, pl) -
+                                       topo_.pinPosition(net, 1, pl)));
+  addPin(topo_.pinCell(net, 0), topo_.pinCell(net, 1), topo_.pinOffset(net, 0),
+         topo_.pinOffset(net, 1), w);
+}
+
+void MatrixCreator::addClique(int net, const std::vector<float> &pl,
+                              float epsilon) {
+  int nb = topo_.nbPins(net);
+  float w = 2.0f * topo_.netWeight(net) / (nb * (nb - 1));
+  for (int i = 0; i + 1 < nb; ++i) {
+    for (int j = i + 1; j < nb; ++j) {
+      float distW =
+          w / std::max(epsilon, std::abs(topo_.pinPosition(net, i, pl) -
+                                         topo_.pinPosition(net, j, pl)));
+      addPin(topo_.pinCell(net, i), topo_.pinCell(net, j),
+             topo_.pinOffset(net, i), topo_.pinOffset(net, j), distW);
+    }
+  }
+}
+
+void MatrixCreator::addStar(int net, const std::vector<float> &pl,
+                            float epsilon) {
+  if (topo_.nbPins(net) <= 2) {
+    addBipoint(net, pl, epsilon);
+  } else {
+    auto [minI, minCell, minOffset, minPos] = topo_.minPin(net, pl);
+    auto [maxI, maxCell, maxOffset, maxPos] = topo_.maxPin(net, pl);
+    float starPos = 0.5f * (minPos + maxPos);
+    int starC = addCell(starPos);
+    for (int i = 0; i < topo_.nbPins(net); ++i) {
+      int c = topo_.pinCell(net, i);
+      float pos = topo_.pinPosition(net, i, pl);
+      if (i == minI || i == maxI) {
+        // Strength of 1 when at the current position
+        float w =
+            topo_.netWeight(net) / std::max(epsilon, std::abs(pos - starPos));
+        addPin(c, starC, topo_.pinOffset(net, i), 0.0f, w);
+      } else {
+        // Strength of 0 when at the current position, 1 when pushed to a
+        // boundary of the net
+        float dist = std::min(maxPos - pos, pos - minPos);
+        float w = topo_.netWeight(net) / std::max(epsilon, dist);
+        addPin(c, starC, topo_.pinOffset(net, i), pos - starPos, w);
+      }
+    }
+  }
+}
+
+void MatrixCreator::addB2B(int net, const std::vector<float> &pl,
+                           float epsilon) {
+  auto [minI, minCell, minOffset, minPos] = topo_.minPin(net, pl);
+  auto [maxI, maxCell, maxOffset, maxPos] = topo_.maxPin(net, pl);
+  float w = topo_.netWeight(net) / (topo_.nbPins(net) - 1);
+  for (int i = 0; i < topo_.nbPins(net); ++i) {
+    float pos = topo_.pinPosition(net, i, pl);
+    int c = topo_.pinCell(net, i);
+    if (i == minI) continue;
+    float distMin = w / std::max(epsilon, std::abs(pos - minPos));
+    addPin(c, minCell, topo_.pinOffset(net, i), minOffset, distMin);
+    if (i == maxI) continue;
+    float distMax = w / std::max(epsilon, std::abs(pos - maxPos));
+    addPin(c, maxCell, topo_.pinOffset(net, i), maxOffset, distMax);
+  }
+}
+
+std::vector<float> MatrixCreator::solve() {
+  check();
+  Eigen::SparseMatrix<float> mat(matSize(), matSize());
+  mat.setFromTriplets(mat_.begin(), mat_.end());
+  Eigen::Map<Eigen::Matrix<float, -1, 1> > rhs(rhs_.data(), rhs_.size());
+  Eigen::Map<Eigen::Matrix<float, -1, 1> > initial(initial_.data(),
+                                                   initial_.size());
+  Eigen::ConjugateGradient<Eigen::SparseMatrix<float> > solver;
+  solver.compute(mat);
+  solver.setTolerance(1.0e-6f);
+  solver.setMaxIterations(1000);
+  Eigen::Matrix<float, -1, 1> res = solver.solveWithGuess(rhs, initial);
+  // Copy to a std::vector and remove the fake cells
+  std::vector<float> ret;
+  ret.resize(matSize());
+  Eigen::Matrix<float, -1, 1>::Map(ret.data(), ret.size()) = res;
+  ret.resize(nbCells_);
+  return ret;
+}
+
+void MatrixCreator::check() const {
+  assert (matSize() == initial_.size());
+  assert (matSize() == rhs_.size());
+}
+
+std::vector<float> NetModel::solveStar() const {
+  MatrixCreator builder = MatrixCreator::createStar(*this);
+  return builder.solve();
+}
+
+std::vector<float> NetModel::solveStar(const std::vector<float> &placement,
+                                       float epsilon) const {
+  MatrixCreator builder = MatrixCreator::createStar(*this, placement, epsilon);
+  return builder.solve();
+}
+
+std::vector<float> NetModel::solveStar(
+    const std::vector<float> &netPlacement, float epsilon,
+    const std::vector<float> &placementTarget,
+    const std::vector<float> &penaltyStrength, float cutoffDistance) const {
+  MatrixCreator builder =
+      MatrixCreator::createStar(*this, netPlacement, epsilon);
+  builder.addPenalty(netPlacement, placementTarget, penaltyStrength,
+                     cutoffDistance);
+  return builder.solve();
+}
+
+std::vector<float> NetModel::solveB2B(const std::vector<float> &placement,
+                                      float epsilon) const {
+  MatrixCreator builder = MatrixCreator::createB2B(*this, placement, epsilon);
+  return builder.solve();
+}
+
+std::vector<float> NetModel::solveB2B(const std::vector<float> &netPlacement,
+                                      float epsilon,
+                                      const std::vector<float> &placementTarget,
+                                      const std::vector<float> &penaltyStrength,
+                                      float cutoffDistance) const {
+  MatrixCreator builder =
+      MatrixCreator::createB2B(*this, netPlacement, epsilon);
+  builder.addPenalty(netPlacement, placementTarget, penaltyStrength,
+                     cutoffDistance);
+  return builder.solve();
 }
---
+++ test/test_net_model.cpp
@@ -0,0 +1,75 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE CPP_API
+
+#include <boost/test/unit_test.hpp>
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <vector>
+
+#include "place_global/net_model.hpp"
+
+BOOST_AUTO_TEST_CASE(b2b_singlepin) {
+  int nbCells = 4;
+  NetModel model(nbCells);
+  model.addNet({0}, {0.0f}, 4.0f, 4.0f);
+  model.addNet({1}, {5.0f}, 1.0f, 1.0f);
+  model.addNet({2}, {-6.0f}, 2.0f, 2.0f);
+  model.addNet({3}, {2.0f}, 3.0f, 3.0f);
+  BOOST_CHECK_EQUAL(model.nbCells(), 4);
+  BOOST_CHECK_EQUAL(model.nbNets(), 4);
+  BOOST_CHECK_EQUAL(model.nbPins(0), 2);
+  BOOST_CHECK_EQUAL(model.nbPins(1), 2);
+  BOOST_CHECK_EQUAL(model.nbPins(2), 2);
+  BOOST_CHECK_EQUAL(model.nbPins(3), 2);
+  std::vector<float> place = {10.0f, 20.0f, 30.0f, 40.0f};
+  auto res = model.solveB2B(place, 1.0e-8);
+  BOOST_CHECK_CLOSE(res[0], 4.0, 0.001);
+  BOOST_CHECK_CLOSE(res[1], -4.0, 0.001);
+  BOOST_CHECK_CLOSE(res[2], 8.0, 0.001);
+  BOOST_CHECK_CLOSE(res[3], 1.0, 0.001);
+}
+BOOST_AUTO_TEST_CASE(b2b_mid) {
+  int nbCells = 2;
+  NetModel model(nbCells);
+  model.addNet({0, 1}, {0.0f, 0.0f}, 0.0f, 4.0f);
+  std::vector<float> place = {2.0f, 3.0f};
+  auto res = model.solveB2B(place, 1.0e-8);
+  BOOST_CHECK_CLOSE(res[0], 2.0, 0.001);
+  BOOST_CHECK_CLOSE(res[1], 3.0, 0.001);
+}
+
+BOOST_AUTO_TEST_CASE(b2b_two_pins) {
+  int nbCells = 1;
+  NetModel model(nbCells);
+  model.addNet({0}, {0.0f}, 0.0f, 4.0f);
+  std::vector<float> place = {8.0f};
+  auto res = model.solveB2B(place, 1.0e-8);
+  BOOST_CHECK_CLOSE(res[0], 8.0 / 3, 0.001);
+}
+
+BOOST_AUTO_TEST_CASE(star_singlepin) {
+  int nbCells = 4;
+  NetModel model(nbCells);
+  model.addNet({0}, {0.0f}, 4.0f, 4.0f);
+  model.addNet({1}, {5.0f}, 1.0f, 1.0f);
+  model.addNet({2}, {-6.0f}, 2.0f, 2.0f);
+  model.addNet({3}, {2.0f}, 3.0f, 3.0f);
+  std::vector<float> place = {10.0f, 20.0f, 30.0f, 40.0f};
+  auto res = model.solveStar(place, 1.0e-8);
+  BOOST_CHECK_CLOSE(res[0], 4.0, 0.001);
+  BOOST_CHECK_CLOSE(res[1], -4.0, 0.001);
+  BOOST_CHECK_CLOSE(res[2], 8.0, 0.001);
+  BOOST_CHECK_CLOSE(res[3], 1.0, 0.001);
+}
+
+BOOST_AUTO_TEST_CASE(star_mid) {
+  int nbCells = 2;
+  NetModel model(nbCells);
+  model.addNet({0, 1}, {0.0f, 0.0f}, 0.0f, 4.0f);
+  std::vector<float> place = {2.0f, 3.0f};
+  auto res = model.solveStar(place, 1.0e-8);
+  BOOST_CHECK_CLOSE(res[0], 2.0, 0.001);
+  BOOST_CHECK_CLOSE(res[1], 3.0, 0.001);
+}
