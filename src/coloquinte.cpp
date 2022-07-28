d6f39901cea18abc0a8a31a70d5a7d3a3bdc24d1
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2022-07-28 12:10:43 +0200

    Get rid of gradient descent code relying on xtensor

--- src/coloquinte.cpp
+++ src/coloquinte.cpp
@@ -7,9 +7,8 @@
 
 #include "place_detailed/place_detailed.hpp"
 #include "place_global/density_legalizer.hpp"
-#include "place_global/gradient_descent.hpp"
 #include "place_global/place_global.hpp"
-#include "place_global/wirelength_model.hpp"
+#include "place_global/net_model.hpp"
 
 extern "C" {
 void place_ispd(int nb_cells, int nb_nets, int *cell_widths, int *cell_heights,
@@ -40,24 +39,24 @@ void benchmark_quadratic_models(int nb_cells, int nb_nets, int *cell_widths,
       nb_cells, nb_nets, cell_widths, cell_heights, cell_fixed, net_limits,
       pin_cells, pin_x_offsets, pin_y_offsets, cell_x, cell_y, cell_flip_x,
       cell_flip_y, 0, NULL, NULL, NULL, NULL);
-  auto xtopo = NetWirelength::xTopology(circuit);
+  auto xtopo = NetModel::xTopology(circuit);
 
-  auto starPlace = xtopo.starSolve();
+  auto starPlace = xtopo.solveStar();
   std::cout << "INIT\t" << epsilon << "\t" << relaxation << "\t" << 0 << "\t"
-            << xtopo.valueHPWL(starPlace) << std::endl;
+            << xtopo.value(starPlace) << std::endl;
   for (int i = 0; i < nb_steps; ++i) {
     if (model_type == 0) {
-      starPlace = xtopo.starSolve(starPlace, epsilon, relaxation, false);
+      starPlace = xtopo.solveStar(starPlace, epsilon);
       std::cout << "STAR\t" << epsilon << "\t" << relaxation << "\t" << i + 1
-                << "\t" << xtopo.valueHPWL(starPlace) << std::endl;
+                << "\t" << xtopo.value(starPlace) << std::endl;
     } else if (model_type == 1) {
-      starPlace = xtopo.starSolve(starPlace, epsilon, relaxation, true);
+      starPlace = xtopo.solveStar(starPlace, epsilon);
       std::cout << "BSTAR\t" << epsilon << "\t" << relaxation << "\t" << i + 1
-                << "\t" << xtopo.valueHPWL(starPlace) << std::endl;
+                << "\t" << xtopo.value(starPlace) << std::endl;
     } else {
-      starPlace = xtopo.b2bSolve(starPlace, epsilon);
+      starPlace = xtopo.solveB2B(starPlace, epsilon);
       std::cout << "B2B\t" << epsilon << "\t" << relaxation << "\t" << i + 1
-                << "\t" << xtopo.valueHPWL(starPlace) << std::endl;
+                << "\t" << xtopo.value(starPlace) << std::endl;
     }
   }
 }
