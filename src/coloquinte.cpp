3808cfb4ea81c9ec27aad3c7420ca54d8fdb4eca
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2022-08-11 12:00:12 +0200

    Remove circuit.*pp files

--- src/coloquinte.cpp
+++ src/coloquinte.cpp
@@ -1,12 +1,166 @@
 
 #include "coloquinte.hpp"
 
+#include <boost/polygon/polygon.hpp>
 #include <iostream>
 
+#include "coloquinte.hpp"
 #include "place_detailed/place_detailed.hpp"
 #include "place_global/density_legalizer.hpp"
 #include "place_global/net_model.hpp"
 #include "place_global/place_global.hpp"
+
+namespace bpl = boost::polygon;
+
+Circuit::Circuit(int nbCells) {
+  cellWidths.resize(nbCells);
+  cellHeights.resize(nbCells);
+  cellFixed.resize(nbCells);
+  cellX.resize(nbCells);
+  cellY.resize(nbCells);
+  cellOrientation.resize(nbCells);
+  netLimits.push_back(0);
+  check();
+}
+
+void Circuit::setNets(const std::vector<int> &limits,
+                      const std::vector<int> &cells,
+                      const std::vector<int> &xOffsets,
+                      const std::vector<int> &yOffsets) {
+  assert(!limits.empty());
+  assert(limits.front() == 0);
+  assert(limits.back() == cells.size());
+  assert(limits.back() == xOffsets.size());
+  assert(limits.back() == yOffsets.size());
+  netLimits = limits;
+  pinCells = cells;
+  pinXOffsets = xOffsets;
+  pinYOffsets = yOffsets;
+}
+
+int Circuit::width(int cell) const {
+  CellOrientation orient = orientation(cell);
+  bool turned = orient == CellOrientation::E || orient == CellOrientation::W ||
+                orient == CellOrientation::FW || orient == CellOrientation::FE;
+  return turned ? cellHeights[cell] : cellWidths[cell];
+}
+
+int Circuit::height(int cell) const {
+  CellOrientation orient = orientation(cell);
+  bool turned = orient == CellOrientation::E || orient == CellOrientation::W ||
+                orient == CellOrientation::FW || orient == CellOrientation::FE;
+  return turned ? cellWidths[cell] : cellHeights[cell];
+}
+
+int Circuit::pinXOffset(int net, int i) const {
+  int cell = pinCell(net, i);
+  CellOrientation orient = orientation(cell);
+  bool turned = orient == CellOrientation::E || orient == CellOrientation::W ||
+                orient == CellOrientation::FW || orient == CellOrientation::FE;
+  int offs = turned ? pinYOffsets[netLimits[net] + i]
+                    : pinXOffsets[netLimits[net] + i];
+  bool flipped = orient == CellOrientation::S || orient == CellOrientation::W ||
+                 orient == CellOrientation::FN || orient == CellOrientation::FE;
+  return flipped ? width(cell) - offs : offs;
+}
+
+int Circuit::pinYOffset(int net, int i) const {
+  int cell = pinCell(net, i);
+  CellOrientation orient = orientation(cell);
+  bool turned = orient == CellOrientation::E || orient == CellOrientation::W ||
+                orient == CellOrientation::FW || orient == CellOrientation::FE;
+  int offs = turned ? pinXOffsets[netLimits[net] + i]
+                    : pinYOffsets[netLimits[net] + i];
+  bool flipped = orient == CellOrientation::S || orient == CellOrientation::E ||
+                 orient == CellOrientation::FS || orient == CellOrientation::FE;
+  return flipped ? height(cell) - offs : offs;
+}
+
+long long Circuit::hpwl() const {
+  long long ret = 0;
+  for (int net = 0; net < nbNets(); ++net) {
+    if (nbPins(net) == 0) {
+      continue;
+    }
+    int minX = std::numeric_limits<int>::max();
+    int maxX = std::numeric_limits<int>::min();
+    int minY = std::numeric_limits<int>::max();
+    int maxY = std::numeric_limits<int>::min();
+    for (int pin = 0; pin < nbPins(net); ++pin) {
+      int cell = pinCell(net, pin);
+      int px = x(cell) + pinXOffset(net, pin);
+      int py = y(cell) + pinYOffset(net, pin);
+      minX = std::min(px, minX);
+      maxX = std::max(px, maxX);
+      minY = std::min(py, minY);
+      maxY = std::max(py, maxY);
+    }
+    ret += (maxX - minX);
+    ret += (maxY - minY);
+  }
+  return ret;
+}
+
+Rectangle Circuit::computePlacementArea() const {
+  int minX = std::numeric_limits<int>::max();
+  int maxX = std::numeric_limits<int>::min();
+  int minY = std::numeric_limits<int>::max();
+  int maxY = std::numeric_limits<int>::min();
+  if (rows.empty()) {
+    return Rectangle(0, 0, 0, 0);
+  }
+  for (Rectangle row : rows) {
+    minX = std::min(row.minX, minX);
+    maxX = std::max(row.maxX, maxX);
+    minY = std::min(row.minY, minY);
+    maxY = std::max(row.maxY, maxY);
+  }
+  return Rectangle(minX, maxX, minY, maxY);
+}
+
+std::vector<Rectangle> Circuit::computeRows() const {
+  std::vector<Rectangle> obstacles;
+  for (int i = 0; i < nbCells(); ++i) {
+    if (!fixed(i)) continue;
+    obstacles.emplace_back(x(i), x(i) + width(i), y(i), y(i) + height(i));
+  }
+  std::vector<Rectangle> ret;
+  for (Rectangle row : rows) {
+    bpl::polygon_90_set_data<int> row_set;
+    row_set.insert(
+        bpl::rectangle_data<int>(row.minX, row.minY, row.maxX, row.maxY));
+    for (Rectangle r : obstacles) {
+      row_set.insert(bpl::rectangle_data<int>(r.minX, r.minY, r.maxX, r.maxY),
+                     true);
+    }
+
+    std::vector<bpl::rectangle_data<int> > diff;
+    bpl::get_rectangles(diff, row_set);
+    for (auto r : diff) {
+      Rectangle newRow(bpl::xl(r), bpl::xh(r), bpl::yl(r), bpl::yh(r));
+      // Filter out partially covered rows
+      if (newRow.height() == row.height()) {
+        ret.push_back(newRow);
+      }
+    }
+  }
+
+  return ret;
+}
+
+void Circuit::check() const {
+  assert(cellWidths.size() == nbCells());
+  assert(cellHeights.size() == nbCells());
+  assert(cellFixed.size() == nbCells());
+  assert(cellX.size() == nbCells());
+  assert(cellY.size() == nbCells());
+  assert(cellOrientation.size() == nbCells());
+  assert(!netLimits.empty());
+  assert(netLimits.front() == 0);
+  assert(pinCells.size() == nbPins());
+  assert(pinXOffsets.size() == nbPins());
+  assert(pinYOffsets.size() == nbPins());
+}
 
 void place(Circuit &circuit, int effort) {
   std::cout << "Placing circuit with " << circuit.nbCells() << " cells, "
