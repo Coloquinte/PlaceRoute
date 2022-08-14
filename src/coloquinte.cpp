55d46edb0f98727b26c03f467c988ada313ce28d
Thomas Kramer <code@tkramer.ch>
2022-08-14 11:23:37 +0200

    Catch C++ exceptions in C interface
    * Handling C++ exceptions from C or some other language is difficult.
    * Use a `int` return value for signalling errors.
    * Catch all C++ exceptions and translate them into a return value of
      `-1`.

--- src/coloquinte.cpp
+++ src/coloquinte.cpp
@@ -176,38 +176,59 @@ void place(Circuit &circuit, int effort) {
 }
 
 extern "C" {
-void place_ispd(int nb_cells, int nb_nets, int *cell_widths, int *cell_heights,
+int place_ispd(int nb_cells, int nb_nets, int *cell_widths, int *cell_heights,
                 char *cell_fixed, int *net_limits, int *pin_cells,
                 int *pin_x_offsets, int *pin_y_offsets, int *cell_x,
                 int *cell_y, int *cell_orientation, int nb_rows, int *row_min_x,
                 int *row_max_x, int *row_min_y, int *row_max_y, int effort) {
+  
   Circuit circuit(nb_cells);
-  circuit.setCellWidths(std::vector<int>(cell_widths, cell_widths + nb_cells));
-  circuit.setCellHeights(
-      std::vector<int>(cell_heights, cell_heights + nb_cells));
-  circuit.setCellFixed(std::vector<char>(cell_fixed, cell_fixed + nb_cells));
-  int nb_pins = net_limits[nb_nets];
-  circuit.setNets(std::vector<int>(net_limits, net_limits + nb_nets + 1),
-                  std::vector<int>(pin_cells, pin_cells + nb_pins),
-                  std::vector<int>(pin_x_offsets, pin_x_offsets + nb_pins),
-                  std::vector<int>(pin_y_offsets, pin_y_offsets + nb_pins));
-  circuit.setCellX(std::vector<int>(cell_x, cell_x + nb_cells));
-  circuit.setCellY(std::vector<int>(cell_y, cell_y + nb_cells));
-  std::vector<CellOrientation> orient;
-  for (int i = 0; i < nb_cells; ++i) {
-    orient.push_back(static_cast<CellOrientation>(cell_orientation[i]));
+    
+  try {
+    
+    circuit.setCellWidths(std::vector<int>(cell_widths, cell_widths + nb_cells));
+    circuit.setCellHeights(
+        std::vector<int>(cell_heights, cell_heights + nb_cells));
+    circuit.setCellFixed(std::vector<char>(cell_fixed, cell_fixed + nb_cells));
+    int nb_pins = net_limits[nb_nets];
+    circuit.setNets(std::vector<int>(net_limits, net_limits + nb_nets + 1),
+                    std::vector<int>(pin_cells, pin_cells + nb_pins),
+                    std::vector<int>(pin_x_offsets, pin_x_offsets + nb_pins),
+                    std::vector<int>(pin_y_offsets, pin_y_offsets + nb_pins));
+    circuit.setCellX(std::vector<int>(cell_x, cell_x + nb_cells));
+    circuit.setCellY(std::vector<int>(cell_y, cell_y + nb_cells));
+    std::vector<CellOrientation> orient;
+    for (int i = 0; i < nb_cells; ++i) {
+      orient.push_back(static_cast<CellOrientation>(cell_orientation[i]));
+    }
+    circuit.setOrientation(orient);
+    std::vector<Rectangle> rows;
+    for (int i = 0; i < nb_rows; ++i) {
+      rows.emplace_back(row_min_x[i], row_max_x[i], row_min_y[i], row_max_y[i]);
+    }
+    circuit.setRows(rows);
+    
+  } catch (const std::exception& e) {
+    std::cout << "initialization terminated with exception: " << e.what() << std::endl;
+    return -1;
   }
-  circuit.setOrientation(orient);
-  std::vector<Rectangle> rows;
-  for (int i = 0; i < nb_rows; ++i) {
-    rows.emplace_back(row_min_x[i], row_max_x[i], row_min_y[i], row_max_y[i]);
+  
+  try {
+    
+    place(circuit, effort);
+  
+  } catch (const std::exception& e) {
+    std::cout << "placement terminated with exception: " << e.what() << std::endl;
+    return -1;
   }
-  circuit.setRows(rows);
-  place(circuit, effort);
+  
   for (int i = 0; i < nb_cells; ++i) {
     cell_x[i] = circuit.cellX[i];
     cell_y[i] = circuit.cellY[i];
   }
+  
+  // Return with success.
+  return 0;
 }
 }
 }
--- src/coloquinte.hpp
+++ src/coloquinte.hpp
@@ -309,7 +309,7 @@ extern "C" {
 /**
  * @brief Ugly interface to easily call Coloquinte from external code
  */
-void place_ispd(int nb_cells, int nb_nets, int *cell_widths, int *cell_heights,
+int place_ispd(int nb_cells, int nb_nets, int *cell_widths, int *cell_heights,
                 char *cell_fixed, int *net_limits, int *pin_cells,
                 int *pin_x_offsets, int *pin_y_offsets, int *cell_x,
                 int *cell_y, int *cell_orientation, int nb_rows, int *row_min_x,
