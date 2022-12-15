ff23be0287c429dc6aa9093690c76159a66dace2
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2022-12-15 17:02:38 +0100

    Update time limit

--- hyperparameter_opt.sh
+++ hyperparameter_opt.sh
@@ -1,5 +1,5 @@
 python3 hyperparameter_opt.py --benchmarks adaptec1 \
-    --time-limit 3600 --time-limit-per-trial 600 \
+    --time-limit 3600 --time-limit-per-trial 2000 \
     --variables \
     global_gap_tolerance global_initial_penalty global_penalty_update_factor \
     global_penalty_cutoff_distance global_approximation_distance \
