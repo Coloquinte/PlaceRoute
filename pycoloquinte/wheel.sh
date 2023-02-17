c5f83544860a6033a5e55b2bf8752afd13e6c87b
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-02-17 14:38:13 +0100

    Use system lemon library as found by CMake

--- pycoloquinte/wheel.sh
+++ pycoloquinte/wheel.sh
@@ -1,6 +1,6 @@
 # Run on docker run -it quay.io/pypa/manylinux2014_x86_64 /bin/bash
 git clone https://github.com/Coloquinte/PlaceRoute --recursive
-yum install boost-devel boost-static eigen3-devel -y
+yum install boost-devel boost-static eigen3-devel coin-or-lemon-devel -y
 cd PlaceRoute/pycoloquinte/
 for p in /usr/local/bin/python3*; do ${p} setup.py bdist_wheel; done
 for w in dist/coloquinte*; do auditwheel repair $w; done
